5c6be016ee2d696b33804ba951202f01c97f37e6
VladKalorad <vladchernav210@gmail.com>
2022-03-24 16:51:48 +0300

    Add image

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: {minimum: 5}
+  mount_uploader :image, ImageUploader
+  validates :title, presence: true, length: {minimum: 15, maximum:100}
+  validates :body,presence: true, length: {minimum: 15, maximum:660}
 
   has_one_attached :avatar
 end
